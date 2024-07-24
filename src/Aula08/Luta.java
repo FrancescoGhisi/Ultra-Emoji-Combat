950f3abc0b10122d8a76841b845d7d62d64a5764
FrancescoGhisi <1072325009@ulife.com.br>
2024-07-23 21:25:19 -0300

    Initial commit

---
+++ src/Aula08/Luta.java
@@ -0,0 +1,89 @@
+package Aula08;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Luta {
+    // Atributos
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovado;
+    // Métodos Especiais
+    private Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    private void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    private Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    private void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    private int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(boolean aprovado) {
+        this.aprovado = aprovado;
+    }
+    // Métodos Públicos
+    public void marcarLuta(Lutador lutador1, Lutador lutador2) {
+        if (Objects.equals(lutador1.getCategoria(), lutador2.getCategoria())
+                && lutador1 != lutador2) {
+            setAprovado(true);
+            setDesafiado(lutador1);
+            setDesafiante(lutador2);
+        } else {
+            setAprovado(false);
+            setDesafiado(null);
+            setDesafiante(null);
+        }
+    }
+
+    public void lutar() {
+        if (isAprovado()) {
+            System.out.println("### DESAFIADO ###");
+            desafiado.apresentar();
+            System.out.println("### DESAFIANTE ###");
+            desafiante.apresentar();
+            Random random = new Random();
+            int Vencedor = random.nextInt(0,3);
+            System.out.println("============== RESULTADO DA LUTA =================");
+            switch (Vencedor) {
+                case 0:
+                    System.out.println("Empatou!");
+                    desafiado.empatarLuta();
+                    desafiante.empatarLuta();
+                break;
+                case 1:
+                    System.out.println("Vitória do " + desafiado.getNome());
+                    desafiado.ganharLuta();
+                    desafiante.perderLuta();
+                break;
+                case 2:
+                    System.out.println("Vitória do " + desafiante.getNome());
+                    desafiado.perderLuta();
+                    desafiante.ganharLuta();
+                break;
+            }
+            System.out.println("==================================================");
+        } else {
+            System.out.println("Luta não pode acontecer.");
+        }
+    }
+}
---
+++ src/Aula08/Lutador.java
@@ -0,0 +1,141 @@
+package Aula08;
+
+public class Lutador {
+    // Atributos
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+    // Métodos Especiais
+    public Lutador(String nome, String nacionalidade, int idade, float altura, float peso,
+                   int vitorias, int derrotas, int empates) {
+        this.setNome(nome);
+        this.setNacionalidade(nacionalidade);
+        this.setIdade(idade);
+        this.setAltura(altura);
+        this.setPeso(peso);
+        this.setVitorias(vitorias);
+        this.setDerrotas(derrotas);
+        this.setEmpates(empates);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    private void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    private void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    private void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    private void setPeso(float peso) {
+        this.peso = peso;
+        setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    private void setCategoria() {
+        if (this.peso < 52.2) {
+            this.categoria = "Inválido";
+        } else if (this.peso <= 70.3) {
+            this.categoria = "Leve";
+        } else if (this.peso <= 83.9) {
+            categoria = "Médio";
+        } else if (this.peso <= 120.2 ) {
+            categoria = "Pesado";
+        } else {
+            categoria = "Inválido";
+        }
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    private void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    private void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    private void setEmpates(int empates) {
+        this.empates = empates;
+    }
+    // Métodos Públicos
+    public void apresentar() {
+        System.out.println("---------------------------------");
+        System.out.println("Lutador: " + this.getNome());
+        System.out.println("Origem: " + this.getNacionalidade());
+        System.out.println(this.getIdade() + " anos");
+        System.out.println("Altura: " + this.getAltura() + "m");
+        System.out.println("Pesando: " + this.getPeso());
+        System.out.println("Ganhou: " + this.getVitorias());
+        System.out.println("Perdeu: " + this.getDerrotas());
+        System.out.println("Empatou: " + this.getEmpates());
+    }
+
+    public void status() {
+        System.out.println(this.getNome());
+        System.out.println("é um peso " + this.getCategoria());
+        System.out.println(this.getVitorias() + " vitórias");
+        System.out.println(this.getDerrotas() + " derrotas");
+        System.out.println(this.getEmpates() + " empates");
+    }
+
+    public void ganharLuta() {
+        this.setVitorias(this.getVitorias() + 1);
+    }
+
+    public void perderLuta() {
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+
+    public void empatarLuta() {
+        this.setEmpates(this.getEmpates() + 1);
+    }
+}
